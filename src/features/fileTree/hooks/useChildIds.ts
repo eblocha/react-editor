3ed4686af9413d4d9b9b9f180da2cbe5fe14763c
eblocha <eblocha@protonmail.com>
2022-03-06 07:47:43 -0500

    use shallowEqual to prevent renders in useChildIds

--- src/features/fileTree/hooks/useChildIds.ts
+++ src/features/fileTree/hooks/useChildIds.ts
@@ -1,10 +1,13 @@
 import { useMemo } from "react";
-import { useSelector } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 import { makeSelectSortedChildren } from "../store";
 import { RootState } from "@/stores";
 
 export const useChildIds = (id?: string) => {
   const selectChildIds = useMemo(() => makeSelectSortedChildren(id), [id]);
 
-  return useSelector((state: RootState) => selectChildIds(state.fileTree));
+  return useSelector(
+    (state: RootState) => selectChildIds(state.fileTree),
+    shallowEqual
+  );
 };
