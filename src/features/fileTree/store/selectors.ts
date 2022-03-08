bd5633f5fa8103e50eb66880e1a2fb44ba90327f
eblocha <eblocha@protonmail.com>
2022-03-07 22:45:57 -0500

    add selectors for listing all ids and paths

--- src/features/fileTree/store/selectors.ts
+++ src/features/fileTree/store/selectors.ts
@@ -1,6 +1,7 @@
 import { getLast } from "@/utils";
 import { createSelector } from "@reduxjs/toolkit";
-import { TreeItem, TreeItems } from "../types";
+import { Directory, TreeItem, TreeItems } from "../types";
+import { appendPath } from "../utils";
 import { FileTreeState } from "./types";
 
 /**
@@ -75,3 +76,106 @@ export const selectAddingToId = (state: FileTreeState, id?: string) => {
   const path = state.addingItem?.path;
   return path ? getLast(path) === id : false;
 };
+
+/**
+ * Determine if a file is the first file inside a dir
+ * @param state The file tree state
+ * @param id The file id
+ * @param dirId The dir id
+ * @returns Whether `id` is the first file under the dir
+ */
+export const selectIsFirstFile = (
+  state: FileTreeState,
+  id: string,
+  dirId?: string
+) => {
+  const dirItem = dirId ? state.dirs[dirId] : state;
+
+  if (!dirItem) return false;
+
+  return dirItem.fileIds[0] === id;
+};
+
+/**
+ * Select ids listed for display
+ * @param state The file tree state
+ * @param dirId select ids for this dir. If undefined, lists all ids for root dir
+ * @returns The ids listed for display
+ */
+export const selectAllIds = (
+  state: FileTreeState,
+  dirId?: string
+): string[] => {
+  const dirItem = dirId ? state.dirs[dirId] : state;
+
+  const ids: string[] = [];
+
+  if (!dirItem) return ids;
+  // Not open - show nothing
+  if (dirId && !(dirItem as Directory).isOpen) return ids;
+
+  for (const id of dirItem.dirIds) {
+    // push the child dir id
+    ids.push(id);
+    // push the child's children
+    ids.push(...selectAllIds(state, id));
+  }
+
+  // push file ids
+  ids.push(...dirItem.fileIds);
+
+  return ids;
+};
+
+/** Make a caching selector for `selectAllIds` */
+export const makeSelectAllIds = () =>
+  createSelector(
+    (state: FileTreeState) => state,
+    (_state: FileTreeState, dirId?: string) => dirId,
+    selectAllIds
+  );
+
+/**
+ * Select list of paths, corresponding to the return value from `selectAllIds`
+ * @param state The file tree state
+ * @param dirId The dir id to select paths for
+ * @param basePath The path of the dir
+ * @returns The paths for each item, corresponding to the return value from `selectAllIds`
+ */
+export const selectAllPaths = (
+  state: FileTreeState,
+  dirId?: string,
+  basePath = ""
+): string[] => {
+  const dirItem = dirId ? state.dirs[dirId] : state;
+
+  // The base path with the new item id appended
+  const newBase = dirId ? appendPath(basePath, dirId) : basePath;
+
+  const paths: string[] = [];
+
+  if (!dirItem) return paths;
+  // Not open - show nothing
+  if (dirId && !(dirItem as Directory).isOpen) return paths;
+
+  for (const id of dirItem.dirIds) {
+    // push the id appended to base path
+    paths.push(appendPath(newBase, id));
+    // push the child's children
+    paths.push(...selectAllPaths(state, id, newBase));
+  }
+
+  // Push files
+  paths.push(...dirItem.fileIds.map((id) => appendPath(newBase, id)));
+
+  return paths;
+};
+
+/** Make a caching selector for `selectAllPaths` */
+export const makeSelectAllPaths = () =>
+  createSelector(
+    (state: FileTreeState) => state,
+    (_state: FileTreeState, dirId?: string) => dirId,
+    (_state: FileTreeState, _dirId?: string, basePath?: string) => basePath,
+    selectAllPaths
+  );
