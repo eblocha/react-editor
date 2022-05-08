9e33ede76818bb5bd153c9028e7f04c2e3780978
eblocha <eblocha@protonmail.com>
2022-05-08 08:56:03 -0400

    add Editor component scaffolding

--- src/views/EditorLayout.tsx
+++ src/views/EditorLayout.tsx
@@ -1,10 +1,12 @@
-import { TabList } from "@/features/editor";
+import { Editor, TabList } from "@/features/editor";
 
 export const EditorLayout = () => {
   return (
     <div className="flex flex-col">
       <TabList />
-      <div className="grow w-full" />
+      <div className="grow w-full">
+        <Editor />
+      </div>
     </div>
   );
 };
