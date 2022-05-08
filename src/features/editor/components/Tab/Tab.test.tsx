e036cdd08b551a27cf03eebfab135894e422c987
eblocha <eblocha@protonmail.com>
2022-05-08 16:13:36 -0400

    only test component rendering in component tests

--- src/features/editor/components/Tab/Tab.test.tsx
+++ src/features/editor/components/Tab/Tab.test.tsx
@@ -1,33 +1,6 @@
-import { initialState, TreeItems } from "@/features/fileTree";
-import { render, screen, fireEvent } from "@/test-utils";
+import { screen, fireEvent } from "@/test-utils";
 import { render as rtlRender } from "@testing-library/react";
-import { Tab, TabInner } from "./Tab";
-
-it("activates a tab when clicked", () => {
-  const id = "test-id";
-  const name = "test-name";
-  const index = 0;
-
-  render(<Tab id={id} index={index} />, {
-    preloadedState: {
-      fileTree: {
-        ...initialState,
-        files: {
-          [id]: {
-            id,
-            name,
-            type: TreeItems.FILE,
-          },
-        },
-      },
-    },
-  });
-
-  const nameElem = screen.getByText(name);
-  expect(screen.getByTestId(`file-tab-${id}`)).not.toHaveClass("bg-white");
-  fireEvent.click(nameElem);
-  expect(screen.getByTestId(`file-tab-${id}`)).toHaveClass("bg-white");
-});
+import { TabInner } from "./Tab";
 
 it("renders a tab and fires events", () => {
   const id = "test-id";
