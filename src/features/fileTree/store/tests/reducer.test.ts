8f5fcd1e536e8e8c487ae21458eada2e05dab831
eblocha <eblocha@protonmail.com>
2022-05-08 14:37:38 -0400

    add tests for moving items in the file tree

--- src/features/fileTree/store/tests/reducer.test.ts
+++ src/features/fileTree/store/tests/reducer.test.ts
@@ -1,5 +1,5 @@
 import { TreeItems } from "../../types";
-import { createDir, createFile } from "../actions";
+import { createDir, createFile, move } from "../actions";
 import { fileTreeReducer, initialState } from "../reducer";
 import { FileTreeState } from "../types";
 
@@ -179,5 +179,181 @@ describe("Adding dirs", () => {
       action1.payload.id,
       action2.payload.id,
     ]);
+  });
+});
+
+describe("moving items", () => {
+  it("moves a dir into its sibling", () => {
+    const action1 = createDir({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = createDir({
+      name: "root2",
+      parent: [],
+    });
+
+    const action3 = move({
+      from: [action2.payload.id],
+      to: [action1.payload.id],
+    });
+
+    const initial = fileTreeReducer(
+      fileTreeReducer(undefined, action1),
+      action2
+    );
+    const newState = fileTreeReducer(initial, action3);
+
+    const expected: FileTreeState = {
+      activeItem: [action1.payload.id, action2.payload.id],
+      addingItem: undefined,
+      dirIds: [action1.payload.id],
+      fileIds: [],
+      files: {},
+      selectionData: {
+        anchor1: null,
+        anchor2: null,
+        selected: {},
+      },
+      dirs: {
+        [action1.payload.id]: {
+          id: action1.payload.id,
+          name: action1.payload.name,
+          dirIds: [action2.payload.id],
+          fileIds: [],
+          // destination should be open
+          isOpen: true,
+          type: TreeItems.DIR,
+        },
+        [action2.payload.id]: {
+          id: action2.payload.id,
+          name: action2.payload.name,
+          dirIds: [],
+          fileIds: [],
+          isOpen: false,
+          type: TreeItems.DIR,
+        },
+      },
+    };
+
+    expect(newState).toStrictEqual(expected);
+  });
+
+  it("no-ops if destination is not a dir", () => {
+    const action1 = createFile({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = createDir({
+      name: "root2",
+      parent: [],
+    });
+
+    const action3 = move({
+      from: [action2.payload.id],
+      to: [action1.payload.id],
+    });
+
+    const initial = fileTreeReducer(
+      fileTreeReducer(undefined, action1),
+      action2
+    );
+    const newState = fileTreeReducer(initial, action3);
+
+    expect(newState).toStrictEqual(initial);
+  });
+
+  it("no-ops if a circular reference would be created", () => {
+    const action1 = createFile({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = createDir({
+      name: "root2",
+      parent: [action1.payload.id],
+    });
+
+    const action3 = move({
+      from: [action1.payload.id],
+      to: [action1.payload.id, action2.payload.id],
+    });
+
+    const initial = fileTreeReducer(
+      fileTreeReducer(undefined, action1),
+      action2
+    );
+    const newState = fileTreeReducer(initial, action3);
+
+    expect(newState).toStrictEqual(initial);
+  });
+
+  it("no-ops when trying to move root", () => {
+    const action1 = createFile({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = move({
+      from: [],
+      to: [action1.payload.id],
+    });
+
+    const initial = fileTreeReducer(undefined, action1);
+    const newState = fileTreeReducer(initial, action2);
+
+    expect(newState).toStrictEqual(initial);
+  });
+
+  it("no-ops if the source does not exist", () => {
+    const action1 = createFile({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = createDir({
+      name: "root2",
+      parent: [action1.payload.id],
+    });
+
+    const action3 = move({
+      from: ["does-not-exist"],
+      to: [action1.payload.id],
+    });
+
+    const initial = fileTreeReducer(
+      fileTreeReducer(undefined, action1),
+      action2
+    );
+    const newState = fileTreeReducer(initial, action3);
+
+    expect(newState).toStrictEqual(initial);
+  });
+
+  it("no-ops if the dest does not exist", () => {
+    const action1 = createFile({
+      name: "root1",
+      parent: [],
+    });
+
+    const action2 = createDir({
+      name: "root2",
+      parent: [action1.payload.id],
+    });
+
+    const action3 = move({
+      from: [action1.payload.id],
+      to: ["does-not-exist"],
+    });
+
+    const initial = fileTreeReducer(
+      fileTreeReducer(undefined, action1),
+      action2
+    );
+    const newState = fileTreeReducer(initial, action3);
+
+    expect(newState).toStrictEqual(initial);
   });
 });
